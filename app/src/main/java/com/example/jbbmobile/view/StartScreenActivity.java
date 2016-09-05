074d5cf247215f1e0a65c807253462df43249a9f
Roger <Rogerlenke@gmail.com>
2016-09-04 22:47:58 -0300

    Making Start Screen-Register Screen transition

--- app/src/main/java/com/example/jbbmobile/view/StartScreenActivity.java
+++ app/src/main/java/com/example/jbbmobile/view/StartScreenActivity.java
@@ -1,7 +1,10 @@
 package com.example.jbbmobile.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.example.jbbmobile.R;
 
@@ -11,5 +14,15 @@ public class StartScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        RelativeLayout normalSingUpRelativeLayout = (RelativeLayout) findViewById(R.id.normalSingUpRelativeLayout);
+
+        normalSingUpRelativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(StartScreenActivity.this, RegisterScreenActivity.class);
+                StartScreenActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
