dbdcb11254e190427c5680001659b9d9b78af048
Roger <Rogerlenke@gmail.com>
2016-09-04 22:34:14 -0300

    Creating Register Screen

---
+++ app/src/main/java/com/example/jbbmobile/view/RegisterScreenActivity.java
@@ -0,0 +1,15 @@
+package com.example.jbbmobile.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jbbmobile.R;
+
+public class RegisterScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+    }
+}
