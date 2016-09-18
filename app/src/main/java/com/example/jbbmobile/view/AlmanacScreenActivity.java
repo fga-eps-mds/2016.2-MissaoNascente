87881b5976d89da8bc2ec3d570cf08892c4878b6
João Paulo <paulonsoares@yahoo.com.br>
2016-09-18 14:52:29 -0300

    Adding books to AlmanacScreenActivity

--- app/src/main/java/com/example/jbbmobile/view/AlmanacScreenActivity.java
+++ app/src/main/java/com/example/jbbmobile/view/AlmanacScreenActivity.java
@@ -1,6 +1,7 @@
 package com.example.jbbmobile.view;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,11 +21,40 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class AlmanacScreenActivity extends AppCompatActivity {
+public class AlmanacScreenActivity extends AppCompatActivity implements View.OnClickListener{
     private EditText date;
     private GridView gridView;
+    private ImageButton imageButton1;
+    private ImageButton imageButton2;
+    private ImageButton imageButton3;
     // private TextView textView;
 
+
+    int[] list = new int[]{R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal,
+            R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal,
+            R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal};
+
+    int[] list2 = new int[]{R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal,
+            R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal,
+            R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal};
+
+    int[] list3 = new int[]{R.drawable.pequi,R.drawable.pequi,R.drawable.pequi,
+            R.drawable.pequi,R.drawable.pequi,R.drawable.pequi,
+            R.drawable.pequi,R.drawable.pequi,R.drawable.pequi};
+
+
+    final String[] web={"Imagem1","Imagem2","Imagem3",
+            "Imagem4","Imagem5","Imagem6",
+            "Imagem7","Imagem8","Imagem9"
+    };
+    final String[] web2={"Imagem1.2","Imagem2.2","Imagem3.2",
+            "Imagem4.2","Imagem5.2","Imagem6.2",
+            "Imagem7.2","Imagem8.2","Imagem9.2"
+    };
+    final String[] web3={"Imagem1.3","Imagem2.3","Imagem3.3",
+            "Imagem4.3","Imagem5.3","Imagem6.3",
+            "Imagem7.3","Imagem8.3","Imagem9.3"
+    };
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +63,21 @@ public class AlmanacScreenActivity extends AppCompatActivity {
         final Context contextAlmanacScreen = getApplicationContext();
         initViews();
 
-        int[] list = new int[]{R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal,R.drawable.btn_google_dark_normal};
 
-        final String[] web={"Imagem1","Imagem2","Imagem3"};
+        switch (currentPeriod()){
+            case 1:
+                gridView.setAdapter(new CustomAdapter(this,web,list));
+                Toast.makeText(getBaseContext(),"PERIODO 1 ATIVO", Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                gridView.setAdapter(new CustomAdapter(this,web2,list));
+                break;
+            case 3:
+                gridView.setAdapter(new CustomAdapter(this,web3,list));
+                break;
 
-        gridView.setAdapter(new CustomAdapter(this,web,list));
+        }
+        //gridView.setAdapter(new CustomAdapter(this,web,list));
 
        /* gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -46,10 +87,30 @@ public class AlmanacScreenActivity extends AppCompatActivity {
         });*/
     }
 
+    public int currentPeriod(){
+        int month;
+        month=Integer.valueOf(systemDate());
+        if(month<10){
+            return 1;
+        }else{
+            return 2;
+        }
+    }
+
     private void initViews(){
         //date = (EditText) findViewById(R.id.dateSystem);
-        gridView = (GridView) findViewById(R.id.gridView);
         // textView =(TextView) findViewById(R.id.textView);
+        gridView = (GridView) findViewById(R.id.gridView);
+
+        imageButton1=(ImageButton) findViewById(R.id.imageButton1);
+        imageButton1.setOnClickListener((View.OnClickListener) this);
+
+        imageButton2=(ImageButton) findViewById(R.id.imageButton2);
+        imageButton2.setOnClickListener((View.OnClickListener) this);
+
+        imageButton3=(ImageButton) findViewById(R.id.imageButton3);
+        imageButton3.setOnClickListener((View.OnClickListener) this);
+
     }
 
     protected void onStart() {
@@ -62,11 +123,11 @@ public class AlmanacScreenActivity extends AppCompatActivity {
         ///OBS: NAO FICARA AQUI.PROVAVELMENTE VAI FICAR NA CONTROLLER BOOK
         // dataSystem = (TextView) findViewById(R.id.catchDate);
         long date = System.currentTimeMillis();
-        SimpleDateFormat sdf = new SimpleDateFormat("d/M/yyyy");
-        String dateString = sdf.format(date);
+        //SimpleDateFormat sdf = new SimpleDateFormat("d/M/yyyy");
+        //String dateString = sdf.format(date);
         //dataSystem.setText(dateString);
-        //SimpleDateFormat mes = new SimpleDateFormat("MM");
-        //String month = mes.format(date);
+        SimpleDateFormat mes = new SimpleDateFormat("MM");
+        String month = mes.format(date);
         //SimpleDateFormat dia = new SimpleDateFormat("d");
         //String day = dia.format(date);
         //int dayNumber = Integer.valueOf(dia.format(date));
@@ -74,7 +135,7 @@ public class AlmanacScreenActivity extends AppCompatActivity {
         //if ( monthNumber == 9 ){
         //  Toast.makeText(getApplicationContext(),"ESTACAO 1", Toast.LENGTH_SHORT).show();
         //}
-        return dateString;
+        return month;
     }
 
     public void onBackPressed() {
@@ -83,5 +144,23 @@ public class AlmanacScreenActivity extends AppCompatActivity {
         AlmanacScreenActivity.this.startActivity(mainScreenIntent);
         finish();
     }
-}
 
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.imageButton1:
+                Toast.makeText(getBaseContext(),"LIVRO 1", Toast.LENGTH_SHORT).show();
+                gridView.setAdapter(new CustomAdapter(this,web,list));
+                break;
+
+            case R.id.imageButton2:
+                Toast.makeText(getBaseContext(),"LIVRO 2", Toast.LENGTH_SHORT).show();
+                gridView.setAdapter(new CustomAdapter(this,web2,list2));
+                break;
+
+            case R.id.imageButton3:
+                Toast.makeText(getBaseContext(),"LIVRO 3", Toast.LENGTH_SHORT).show();
+                gridView.setAdapter(new CustomAdapter(this,web3,list3));
+                break;
+        }
+    }
+}
