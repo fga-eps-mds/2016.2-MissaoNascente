67b7887ef2ac7129d9537b09ef6ce2fa9e33402f
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2016-09-20 19:03:53 -0300

    Adding elements pictures

--- app/src/main/java/com/example/jbbmobile/controller/Element.java
+++ app/src/main/java/com/example/jbbmobile/controller/Element.java
@@ -56,7 +56,7 @@ public class Element {
         description.add("Os frutos começam a abrir no final de agosto e as sementes aladas podem ser vistas, sua forma alada facilita a dispersão pelo vento.");
         description.add("Sua reprodução é sincronizada com o período chuvoso, ou seja,");
         description.add("os frutos maduros no final da seca lançam suas sementes que, com a chegada da chuva, tem mais chances de germinação.");
-        setElement(new Elements(0, 0, 100, "btn_google_dark_normal", "Pau-Santo", "null", 1, 1, description));
+        setElement(new Elements(0, 0, 100, "ponto_2", "Pau-Santo", "null", 1, 1, description));
         elementDao.insertInformation(getElement());
         elementDao.insertDescription(getElement());
         elementDao.insertElement(getElement());
@@ -71,7 +71,7 @@ public class Element {
         description.add("podem ser observadas de novembro a maio e atraem vários tipos de insetos para fazer sua polinização, inclusive, abelhas grandes e pequenas.");
         description.add("Seus frutos que aparecem de maio a julho, têm uma alta viabilidade, ou seja, alta taxa de germinação,");
         description.add("podendo ficar por meses em dormência, e germinarem quando as primeiras chuvas caem. Possui frutos secos dispersos pelo vento.");
-        setElement(new Elements(1, 0, 100, "btn_google_dark_normal", "Jacarandá do Cerrado", "Jacaranda", 1, 2, description));
+        setElement(new Elements(1, 0, 100, "ponto_3", "Jacarandá do Cerrado", "Jacaranda", 1, 2, description));
         elementDao.createTablesIfTheyDoesntExist(elementDao.getWritableDatabase());
         elementDao.insertInformation(getElement());
         elementDao.insertDescription(getElement());
@@ -85,7 +85,7 @@ public class Element {
         description.add("Essa espécie apresenta folhas alternas, compostas palmadas. Árvore melífera belas flores claras, polinizada por abelhas.");
         description.add("A paina que é produzida no fruto, quando maduro e aberto,");
         description.add("é utilizada por aves para fazerem seus ninhos e também serve como recheio de travesseiros.");
-        setElement(new Elements(2, 0, 100, "btn_google_dark_normal", "Paineira do Cerrado", "Paineira", 2, 3, description));
+        setElement(new Elements(2, 0, 100, "ponto_5", "Paineira do Cerrado", "Paineira", 2, 3, description));
         elementDao.createTablesIfTheyDoesntExist(elementDao.getWritableDatabase());
         elementDao.insertInformation(getElement());
         elementDao.insertDescription(getElement());
@@ -100,7 +100,7 @@ public class Element {
         description.add("Os termiteiros servem de abrigo a uma fauna diversa, incluindo artrópodes, vertebrados e outros grupos.");
         description.add("Os ninhos velhos e abandonados servem de substrato para o desenvolvimento de várias de plantas.");
         description.add("Devido a esse poder de modificar a estrutura do habitat, os cupins podem ser incluídos entre os “engenheiros do ecossistema”.");
-        setElement(new Elements(3, 0, 100, "btn_google_dark_normal", "Cupinzeiro terrestre", "Cupim", 2, 4, description));
+        setElement(new Elements(3, 0, 100, "ponto_6", "Cupinzeiro terrestre", "Cupim", 2, 4, description));
         elementDao.createTablesIfTheyDoesntExist(elementDao.getWritableDatabase());
         elementDao.insertInformation(getElement());
         elementDao.insertDescription(getElement());
@@ -113,7 +113,7 @@ public class Element {
         description.add("típicas de riachos que se mantém em seu estado primitivo.");
         description.add("Ao se observar essas nascentes deve-se expandir o olhar para");
         description.add("o ecossistema que integra os compartimentos terrestres e aquáticos, conhecidos como ecossistemas ripários.");
-        setElement(new Elements(4, 0, 100, "btn_google_dark_normal", "Nascente do córrego Cabeça de Veado", "Nascente", 3, 5, description));
+        setElement(new Elements(4, 0, 100, "ponto_23", "Nascente do córrego Cabeça de Veado", "Nascente", 3, 5, description));
         elementDao.createTablesIfTheyDoesntExist(elementDao.getWritableDatabase());
         elementDao.insertInformation(getElement());
         elementDao.insertDescription(getElement());
@@ -125,17 +125,17 @@ public class Element {
         description.add("Essas estruturas têm como principal função fixar a planta,");
         description.add("enrolando-se sobre suportes. Muitas por serem verdes, contendo clorofila,");
         description.add("além de fixar auxiliam no processo de fotossíntese.");
-        setElement(new Elements(5, 0, 100, "btn_google_dark_normal", "Gavinha", "Gavinha", 3, 6, description));
+        setElement(new Elements(5, 0, 100, "ponto_34", "Gavinha", "Gavinha", 3, 6, description));
         elementDao.createTablesIfTheyDoesntExist(elementDao.getWritableDatabase());
         elementDao.insertInformation(getElement());
         elementDao.insertDescription(getElement());
         elementDao.insertElement(getElement());
 
-        setElement(new Elements(6, 0, 100, "btn_google_dark_normal", "Caqui", "pequi", 4, 1, description));
+        setElement(new Elements(6, 0, 100,  "ponto_2", "Pau-Santo", "null",4, 1, description));
         elementDao.createTablesIfTheyDoesntExist(elementDao.getWritableDatabase());
         elementDao.insertElement(getElement());
 
-        setElement(new Elements(7, 0, 100, "btn_google_dark_normal", "Caqui", "pequi", 5, 3, description));
+        setElement(new Elements(7, 0, 100, "ponto_5", "Paineira do Cerrado", "Paineira", 5, 3, description));
         elementDao.createTablesIfTheyDoesntExist(elementDao.getWritableDatabase());
         elementDao.insertElement(getElement());
 
