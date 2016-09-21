af5414df5fb2c7ac69bb82089ce25896e89246c6
Renata Soares dos Santos <renata.sds09@gmail.com>
2016-09-20 21:23:53 -0300

    Testing Register User

--- app/src/androidTest/java/com/example/jbbmobile/ExplorerAcceptanceTest.java
+++ app/src/androidTest/java/com/example/jbbmobile/ExplorerAcceptanceTest.java
@@ -14,6 +14,18 @@ import android.support.test.runner.AndroidJUnit4;
 import com.example.jbbmobile.controller.Register;
 import com.example.jbbmobile.view.RegisterScreenActivity;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.doubleClick;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isSelected;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 @RunWith(AndroidJUnit4.class)
 
 @LargeTest
@@ -21,6 +33,18 @@ public class ExplorerAcceptanceTest{
     @Rule public final ActivityTestRule<RegisterScreenActivity> register = new ActivityTestRule<>(RegisterScreenActivity.class);
 
     @Test
-    public void emptyTest(){
+    public void registerUser(){
+        onView(withId(2131427442))
+                .perform(typeText("testuser"));
+        onView(withId(2131427427))
+                .perform(typeText("senha1234"));
+        onView(withId(2131427443))
+                .perform(typeText("senha1234"));
+        onView(withId(2131427426))
+                .perform(typeText("testuser@gmail.com"))
+                .perform(closeSoftKeyboard());
+        onView(withId(2131427444))
+                .perform(click())
+                .check(matches(isDisplayed()));
     }
 }
