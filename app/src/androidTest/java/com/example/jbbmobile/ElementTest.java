d31021e10ed96620394c9bb59989e8aec2e93fb2
Igor Gabriel <igorgabriel515@gmail.com>
2016-10-16 18:41:55 -0200

    Adding Test Element
    Signed-off-by: Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
    Signed-off-by: Igor Gabriel <igorgabriel515@gmail.com>

--- app/src/androidTest/java/com/example/jbbmobile/ElementTest.java
+++ app/src/androidTest/java/com/example/jbbmobile/ElementTest.java
@@ -4,6 +4,7 @@ import com.example.jbbmobile.model.Element;
 
 import org.junit.Test;
 
+import static android.R.attr.id;
 import static org.junit.Assert.*;
 
 public class ElementTest {
@@ -12,94 +13,202 @@ public class ElementTest {
 
     @Test
     public void testIfElementIsCreated() throws Exception{
-        element = new Element(0, 1, 100, "ponto_2", "Pau-Santo", "Pau-Santo", 1, "");
-        assertEquals(0 , element.getIdElement());
+        try {
+            element = new Element(1, 1, 100, "ponto_2", "Pau-Santo", "Pau-Santo", 1, "");
+            assertEquals(1, element.getIdElement());
+        }catch(Exception elementException){
+            elementException.printStackTrace();
+        }
     }
 
     @Test
-    public void testIfElementIsCreatedWithoutIdBookandDescription() throws Exception{
-        element = new Element(0, 1, 100, "ponto_2", "Pau-Santo", "Pau-Santo", 1, "");
-        assertEquals(0 , element.getIdElement());
+    public void testIfElementIsCreatedWithoutIdBookAndDescription() throws Exception{
+        try {
+            element = new Element(1, 1, 100, "ponto_2", "Pau-Santo", "Pau-Santo", 1, "");
+            assertEquals(1, element.getIdElement());
+        }catch(Exception elementException){
+            elementException.printStackTrace();
+        }
     }
 
     @Test
     public void testIfElementIsCreatedWitOnlyIdElementQrCodeNumberElementScoreDefaultImageAndNameElement() throws Exception{
-        element = new Element(0, 0, 100, "btn_google_dark_normal","beterraba");
-        assertEquals(0 , element.getIdElement());
+        try {
+            element = new Element(1, 0, 100, "btn_google_dark_normal", "beterraba");
+            assertEquals(1, element.getIdElement());
+        }catch(Exception elementException){
+            elementException.printStackTrace();
+        }
     }
 
     @Test
     public void testIfIdElementIsLargerThan100() throws Exception{
+        boolean invalid = false;
+
         try{
             element = new Element(101, 0, 100, "btn_google_dark_normal","beterraba");
         }catch (IllegalArgumentException idElementException){
-            assertEquals(idElementException.getMessage(), "Invalid element's id");
+            invalid = idElementException.getMessage().equals("Invalid element's id");
+        }catch (Exception idElementException){
+            idElementException.printStackTrace();
         }
-    }
-    @Test
-    public void testIfQrCodeNumberIsSmallerThanZero() throws Exception{
-        try{
-            element = new Element(1, -1, 100, "btn_google_dark_normal","beterraba");
-        }catch (IllegalArgumentException qrCodeNumberException){
-            assertEquals(qrCodeNumberException.getMessage(), "Invalid QRCode");
-        }
+
+        assertTrue(invalid);
     }
 
     @Test
-    public void testIfNameElementLengthLessThan10() throws Exception{
-        try{
-            element = new Element(1, 0, 100, "btn_google_dark_normal","beterrababeterraba");
-        }catch (IllegalArgumentException nameElementException){
-            assertEquals(nameElementException.getMessage(), "Invalid element's name");
-        }
-    }
-    @Test
     public void testIfIdElementIsSmallerThanZero() throws Exception{
+        boolean invalid = false;
+
         try{
             element = new Element(-1, 0, 100, "btn_google_dark_normal","beterraba");
         }catch (IllegalArgumentException idElementException){
-            assertEquals(idElementException.getMessage(), "Invalid element's id");
+            invalid = idElementException.getMessage().equals("Invalid element's id");
+        }catch (Exception idElementException){
+            idElementException.printStackTrace();
         }
+
+        assertTrue(invalid);
+    }
+
+    @Test
+    public void testIfQrCodeNumberIsSmallerThanZero() throws Exception{
+        boolean invalid = false;
+
+        try{
+            element = new Element(1, -1, 100, "btn_google_dark_normal","beterraba");
+        }catch (IllegalArgumentException qrCodeNumberException){
+            invalid = qrCodeNumberException.getMessage().equals("Invalid QRCode");
+        }catch (Exception qrCodeNumberException){
+            qrCodeNumberException.printStackTrace();
+        }
+
+        assertTrue(invalid);
+    }
+
+    @Test
+    public void testIfNameElementLengthMoreThan80() throws Exception{
+        boolean invalid = false;
+
+        try{
+            element = new Element(1, 0, 100, "ponto_2","TesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTeste");
+        }catch (IllegalArgumentException nameElementException){
+            invalid = nameElementException.getMessage().equals("Invalid element's name");
+        }catch (Exception nameElementException){
+            nameElementException.printStackTrace();
+        }
+
+        assertTrue(invalid);
+    }
+
+    @Test
+    public void testIfNameElementIsNull() throws Exception{
+        boolean invalid = false;
+
+        try{
+            element = new Element(1, 0, 100, "ponto_2", null);
+        }catch (IllegalArgumentException nameElementException){
+            invalid = nameElementException.getMessage().equals("Invalid element's name");
+        }catch (Exception nameElementException){
+            nameElementException.printStackTrace();
+        }
+
+        assertTrue(invalid);
+    }
+
+    @Test
+    public void testIfDefaultImageIsNull() throws Exception{
+        boolean invalid = false;
+
+        try{
+            element = new Element(1, 0, 100, null, "Teste");
+        }catch (IllegalArgumentException defaultImageException){
+            invalid = defaultImageException.getMessage().equals("Invalid element's default image");
+        }catch (Exception defaultImageException){
+            defaultImageException.printStackTrace();
+        }
+
+        assertTrue(invalid);
     }
 
     @Test
     public void testIfIdBookIsCreated() throws Exception{
-        element = new Element(0, 1, 100, "ponto_2", "Pau-Santo", "Pau-Santo", 1, "");
-        assertEquals(1 , element.getIdBook());
+       try {
+           element = new Element(0, 1, 100, "ponto_2", "Pau-Santo", "Pau-Santo", 1, "");
+           assertEquals(1, element.getIdBook());
+       }catch (Exception idBookException){
+           idBookException.printStackTrace();
+       }
     }
 
     @Test
     public void testIfIdBookIsSmallerThanZero() throws Exception{
+        boolean invalid = false;
 
         try {
             element = new Element(0, 1, 100, "ponto_2", "Pau-Santo", "Pau-Santo", -1, "");
         }catch (IllegalArgumentException idBookException) {
-            assertEquals(idBookException.getMessage(), "Invalid id book: -1");
+            invalid = idBookException.getMessage().equals("Invalid id book: -1");
+        }catch (Exception idBookException){
+            idBookException.printStackTrace();
         }
+
+        assertTrue(invalid);
     }
 
     @Test
     public void testIfIdBookIsLargerThan3() throws Exception{
+        boolean invalid = false;
+
         try {
             element = new Element(0, 1, 100, "ponto_2", "Pau-Santo", "Pau-Santo", 4, "");
         }catch (IllegalArgumentException idBookException) {
-            assertEquals(idBookException.getMessage(), "Invalid id book: 4");
+            invalid = idBookException.getMessage().equals("Invalid id book: 4");
+        }catch (Exception idBookException){
+            idBookException.printStackTrace();
         }
+
+        assertTrue(invalid);
+    }
+
+    @Test
+    public void testIfTextDescriptionIsNull() throws Exception{
+        boolean invalid = false;
+
+        try {
+            element = new Element(0, 1, 100, "ponto_2", "Pau-Santo", "Pau-Santo", 1, null);
+        }catch (IllegalArgumentException textDescriptionException) {
+            invalid = textDescriptionException.getMessage().equals("Invalid element's description");
+        }catch (Exception textDescriptionException){
+            textDescriptionException.printStackTrace();
+        }
+
+        assertTrue(invalid);
     }
 
     @Test
     public void testIfElementScoreIsInvalid() throws Exception{
+        boolean invalid = false;
+
         try {
             element = new Element(0, 1, -1, "ponto_2", "Pau-Santo", "Pau-Santo", 1, "");
         }catch (IllegalArgumentException elementScoreException) {
-            assertEquals(elementScoreException.getMessage(), "Invalid element's score");
+            invalid = elementScoreException.getMessage().equals("Invalid element's score");
+        }catch (Exception elementScoreException){
+            elementScoreException.printStackTrace();
         }
+
+        assertTrue(invalid);
     }
 
     @Test
     public void testIfCoordinateIsInsert() throws Exception{
-        element = new Element(0, 1, 100, "ponto_2", "Pau-Santo", "Pau-Santo", 1, "",15.123f,14.123f);
-        assertEquals(0 , element.getIdElement());
+       try {
+           element = new Element(0, 1, 100, "ponto_2", "Pau-Santo", "Pau-Santo", 1, "", 15.123f, 14.123f);
+           assertEquals(0, element.getIdElement());
+       }catch (Exception coordinateInsertException){
+           coordinateInsertException.printStackTrace();
+       }
     }
 
 }
