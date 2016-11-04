d71ca4ffa4dca8ddcfb5f5ac6dc0fd570068c794
Roger Lenke <rogerlenke@gmail.com>
2016-11-04 15:10:23 +0000

    Creating Alternative Test
    Signed-off-by: AllanNobre <allanjefrey9@hotmail.com>
    Signed-off-by: Roger Lenke <rogerlenke@gmail.com>

---
+++ app/src/androidTest/java/com/example/jbbmobile/AlternativeTest.java
@@ -0,0 +1,44 @@
+package com.example.jbbmobile;
+
+import com.example.jbbmobile.model.Alternative;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import static junit.framework.Assert.assertEquals;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class AlternativeTest {
+
+    private final String ALTERNATIVE_DESCRIPTION = "Description";
+    private final String ALTERNATIVE_LETTER = "c";
+    private final int ALTERNATIVE_ID = 1;
+    private final int QUESTION_ID = 1;
+    private Alternative alternative;
+    @Before
+    public void setup(){
+        alternative = new Alternative(ALTERNATIVE_ID, ALTERNATIVE_LETTER, ALTERNATIVE_DESCRIPTION, QUESTION_ID);
+    }
+
+    @Test
+    public void testIfAlternativeWasCreatedWithId(){
+        assertEquals(ALTERNATIVE_ID, alternative.getIdAlternative());
+    }
+
+    @Test
+    public void testIfAlternativeWasCreatedWithLetter(){
+        assertEquals(ALTERNATIVE_LETTER, alternative.getAlternativeLetter());
+    }
+
+    @Test
+    public void testIfAlternativeWasCreatedWithDescription(){
+        assertEquals(ALTERNATIVE_DESCRIPTION, alternative.getAlternativeDescription());
+    }
+
+    @Test
+    public void testIfAlternativeWasCreatedWithQuestionId(){
+        assertEquals(QUESTION_ID, alternative.getIdQuestion());
+    }
+}
