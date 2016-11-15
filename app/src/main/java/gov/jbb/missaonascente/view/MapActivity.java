a77d379fe67faaa6ba4cee62a0137bf0e627e0ef
Roger Lenke <rogerlenke@gmail.com>
2016-11-15 23:30:57 +0000

    Creating map screen
    Signed-off-by: Sabryna Sousa <sabryna.sousa1323@gmail.com>
    Signed-off-by: Roger Lenke <rogerlenke@gmail.com>

---
+++ app/src/main/java/gov/jbb/missaonascente/view/MapActivity.java
@@ -0,0 +1,68 @@
+package gov.jbb.missaonascente.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.ContextThemeWrapper;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import gov.jbb.missaonascente.R;
+import gov.jbb.missaonascente.controller.MainController;
+
+import static java.security.AccessController.getContext;
+
+public class MapActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageButton chooseBookbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        chooseBookbutton = (ImageButton) findViewById(R.id.chooseBookButton);
+        chooseBookbutton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.chooseBookButton:
+                showPopup(findViewById(R.id.chooseBookButton));
+        }
+    }
+
+    private void showPopup(View v) {
+        Context layout = new ContextThemeWrapper(this, R.style.popupMenuStyle);
+        PopupMenu popupMenu = new PopupMenu(layout, v);
+        popupMenu.inflate(R.menu.map_menu);
+
+        MainController mainController = new MainController();
+        mainController.forceImageIcons(popupMenu);
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.water:
+                        //exchange image set to water book
+                        return true;
+                    case R.id.dormancy:
+                        //exchange image set to dormancy book
+                        return true;
+                    case R.id.renovation:
+                        //exchange image set to renovation
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        popupMenu.show();
+    }
+}
