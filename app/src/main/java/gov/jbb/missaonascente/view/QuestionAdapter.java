2d2b81f8532507bbf29d7315d9114411ebbcd254
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2016-11-15 19:02:37 -0200

    Changing color of explorer answer
    Signed-off-by: João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
    Signed-off-by: Igor Gabriel <igorgabriel515@gmail.com>

--- app/src/main/java/gov/jbb/missaonascente/view/QuestionAdapter.java
+++ app/src/main/java/gov/jbb/missaonascente/view/QuestionAdapter.java
@@ -1,6 +1,8 @@
 package gov.jbb.missaonascente.view;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +11,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import gov.jbb.missaonascente.R;
+import gov.jbb.missaonascente.controller.EnergyController;
+import gov.jbb.missaonascente.controller.LoginController;
 import gov.jbb.missaonascente.model.Alternative;
+import gov.jbb.missaonascente.model.Explorer;
 import gov.jbb.missaonascente.model.Question;
 
 import java.util.List;
@@ -18,11 +23,20 @@ public class QuestionAdapter extends BaseAdapter {
     private static LayoutInflater inflater = null;
     private Question question;
     private Context context;
+    private TextView itemQuestion;
+    private Explorer explorer = new Explorer();
+    private LoginController loginController = new LoginController();
+    private MainScreenActivity mainScreenActivity;
+    private EnergyController energyController;
+    private Integer energyQuestion = 10;
     
     public QuestionAdapter(Context context, Question question){
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.question = question;
         this.context = context;
+        explorer = loginController.getExplorer();
+        energyController = new EnergyController(context);
+        mainScreenActivity = new MainScreenActivity();
     }
 
     @Override
@@ -44,8 +58,10 @@ public class QuestionAdapter extends BaseAdapter {
     public View getView(final int position, View view, ViewGroup viewGroup) {
         View listItem;
         listItem = inflater.inflate(R.layout.question_adapter,null);
+        itemQuestion = (TextView) listItem.findViewById(R.id.choice);
+        final int item =0;
 
-        Holder holder = new Holder();
+        final Holder holder = new Holder();
         holder.alternativeText = (TextView) listItem.findViewById(R.id.choice);
         final List<Alternative> alternativeList = question.getAlternativeList();
         holder.alternativeText.setText(alternativeList.get(position).getAlternativeDescription());
@@ -55,12 +71,30 @@ public class QuestionAdapter extends BaseAdapter {
             public void onClick(View v) {
                 String userAnswer = alternativeList.get(position).getAlternativeLetter();
                 String correctAnswer = question.getCorrectAnswer();
+
                 if(userAnswer.equals(correctAnswer)){
+                    int explorerEnergy;
+                    int explorerEnergyText;
+                    holder.alternativeText.setBackgroundColor(Color.parseColor("#32CD32"));
                     Toast.makeText(context, "Parabéns, você acertou!", Toast.LENGTH_SHORT).show();
+                    explorerEnergy = energyController.getExplorer().getEnergy() ;
+                    explorerEnergyText= explorerEnergy+energyQuestion;
+                    //explorer.setEnergy(explorerEnergyText);
+                    energyController.setExplorerEnergyInDataBase(energyController.getExplorer().getEnergy(),energyQuestion);
+
+                    Log.i("QUEST ENERGY explorer",""+explorerEnergy);
+                    Log.i("QUEST ENERGY explorer2",""+energyController.getExplorer().getEnergy());
+                    Log.i("QUEST ENERGY exp+quest",""+explorerEnergyText);
+                    Log.i("QUEST ENERGY EXP",energyController.getExplorer().getEmail());
+
+                    //mainScreenActivity.modifyEnergy();
+                    //mainScreenActivity.updateEnergyProgress();
                 }else{
+                    holder.alternativeText.setBackgroundColor(Color.parseColor("#FF0000"));
                     Toast.makeText(context, "Parabéns, você errou!", Toast.LENGTH_SHORT).show();
                 }
             }
+
         });
         return listItem;
     }
