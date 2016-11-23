b2de95cba8782c0b27619ef1d605c93fb9fc83f9
Hugo Alves <hugo.asb96@gmail.com>
2016-11-23 18:30:48 -0200

    Adding texts to String Values
    Signed-off-by: AllanNobre <allanjefrey9@hotmail.com>
    Signed-off-by: Hugo Alves <hugo.asb96@gmail.com>

--- app/src/main/java/gov/jbb/missaonascente/view/TutorialScreenActivity.java
+++ app/src/main/java/gov/jbb/missaonascente/view/TutorialScreenActivity.java
@@ -61,107 +61,107 @@ public class TutorialScreenActivity extends AppCompatActivity implements View.On
         switch (passages){
             case 0:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.main_screen_image, null));
-                moveShowCaseCircle(energyBarTutorial,"Barra de Energia","Essa é a sua energia, acompanhe quantos elemento pode registrar através dela!\nLembre-se que quando estiver sem energia poderá responder uma questão para tentar recuperar uma parte dela!",ShowcaseView.BELOW_SHOWCASE);
+                moveShowCaseCircle(energyBarTutorial,getString(R.string.energyBar),getString(R.string.energyBarTutorial),ShowcaseView.BELOW_SHOWCASE);
                 break;
             case 1:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.question_image, null));
-                moveShowCaseCircle(questionTutorial,"Questão","Aqui você pode responder uma questão para tentar recuperar parte da energia perdida, caso esteja com menos que o necessário para registrar um elemento!",ShowcaseView.BELOW_SHOWCASE);
+                moveShowCaseCircle(questionTutorial,getString(R.string.question),getString(R.string.questionTutorial),ShowcaseView.BELOW_SHOWCASE);
                 break;
             case 2:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.main_screen_image, null));
-                moveShowCaseCircle(registerTutorial,"Botão de Registro","Aqui você pode registar os elementos! Lembre-se que perde-se energia para cada elemento registrado, tendo ele ou não!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(registerTutorial,getString(R.string.registerTutorialTitle),getString(R.string.registerTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 3:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.registered_element_image, null));
-                moveShowCaseCircle(registeredElementTutorial,"Elemento Registrado","Aqui você pode visualizar o elemento registrado! Lembre-se que ganha-se uma pontuação ao registrar o elemento pela primeira vez!",ShowcaseView.BELOW_SHOWCASE);
+                moveShowCaseCircle(registeredElementTutorial,getString(R.string.registeredElement),getString(R.string.registeredElementTutorial),ShowcaseView.BELOW_SHOWCASE);
                 break;
             case 4:
-                moveShowCaseCircle(takePictureTutorial,"Tirar fotografia","Aqui você pode tirar uma fotografia do elemento registrado!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(takePictureTutorial,getString(R.string.takePicture),getString(R.string.takePictureTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 5:
-                moveShowCaseCircle(moreInformationElementTutorial,"Mais informações","Aqui você pode obter mais informações sobre esse elemento!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(moreInformationElementTutorial,getString(R.string.moreInformationElement),getString(R.string.moreInformationElementTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 6:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.history_element_image, null));
-                moveShowCaseCircle(historyElementTutorial,"Elemento da história","Ao registrar um elemento da história, ele é mostrado de maneira especial!",ShowcaseView.BELOW_SHOWCASE);
+                moveShowCaseCircle(historyElementTutorial,getString(R.string.historyElement),getString(R.string.historyElementTutorial),ShowcaseView.BELOW_SHOWCASE);
                 break;
             case 7:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.main_screen_image, null));
-                moveShowCaseCircle(leafScoreTutorial,"Pontuação","Aqui você pode acompanhar sua quantidade de pontos, ou seja, a soma de todos os pontos obtidos no decorrer da trilha!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(leafScoreTutorial,getString(R.string.leafScore),getString(R.string.leafScoreTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 8:
-                moveShowCaseCircle(mapTutorial,"Mapa de Progresso","Aqui você pode acompanhar seu progresso perante o elementos da história durante a trillha de acordo com o período!\nLembre-se que ele só aparece no momento em que você registrar o primeiro elemento da história!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(mapTutorial,getString(R.string.map),getString(R.string.mapTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 9:
-                moveShowCaseCircle(almanacTutorial,"Almanaque","Aqui você pode acessar todos os seus elementos já registrados, de modo organizado em seus respectivos livros!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(almanacTutorial,getString(R.string.almanac),getString(R.string.almanacTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 10:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.book1_image, null));
-                moveShowCaseCircle(almanacBook1Tutorial,"Livros do Almanaque","Aqui você pode acessar todos os 3 livros que compõem o Almanaque!\nLembre-se que eles representam períodos do ano, nos quais os elementos de cada um são exclusivos do livro ao qual pertencem!",ShowcaseView.BELOW_SHOWCASE);
+                moveShowCaseCircle(almanacBook1Tutorial,getString(R.string.almanacBook1),getString(R.string.almanacBook1Tutorial),ShowcaseView.BELOW_SHOWCASE);
                 break;
             case 11:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.book2_image, null));
-                moveShowCaseCircle(almanacBook2Tutorial,"Livros do Almanaque","Pode-se navegar entre os livros e checar todos os elementos já registrados no período desejado!",ShowcaseView.BELOW_SHOWCASE);
+                moveShowCaseCircle(almanacBook2Tutorial,getString(R.string.almanacBook2),getString(R.string.almanacBook2Tutorial),ShowcaseView.BELOW_SHOWCASE);
                 break;
             case 12:
-                moveShowCaseCircle(elementIconTutorial,"Elemento","Aqui você pode acessar mais informações sobre um elemento em específico!",ShowcaseView.BELOW_SHOWCASE);
+                moveShowCaseCircle(elementIconTutorial,getString(R.string.elementIcon),getString(R.string.elementIconTutorial),ShowcaseView.BELOW_SHOWCASE);
                 break;
             case 13:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.element_book_image, null));
-                moveShowCaseCircle(elementTutorial,"Elemento","Ao clicar no elemento, você poderá contemplar tais informações, assim como uma imagem do elemento registrado!",ShowcaseView.BELOW_SHOWCASE);
+                moveShowCaseCircle(elementTutorial,getString(R.string.element),getString(R.string.elementTutorial),ShowcaseView.BELOW_SHOWCASE);
                 break;
             case 14:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.element_catch_date_book_image, null));
-                moveShowCaseCircle(elementCatchDateTutorial,"Data de Registro","Aqui é possível visualizar a data em que você registrou esse elemento!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(elementCatchDateTutorial,getString(R.string.elementCatchDate),getString(R.string.elementCatchDateTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 15:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.main_screen_image, null));
-                moveShowCaseCircle(menuOptionTutorial,"Mais Opções","Aqui você pode acessar diversas opções que o aplicativo te oferece, como:",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(menuOptionTutorial,getString(R.string.menuOption),getString(R.string.menuOptionTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 16:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.open_menu_image, null));
-                moveShowCaseCircle(achievementOptionTutorial,"Conquistas","Aqui você tem acesso conquistas adquiridas ao alcançar determinados objetivos na trilha!",ShowcaseView.BELOW_SHOWCASE);
+                moveShowCaseCircle(achievementOptionTutorial,getString(R.string.achievementOption),getString(R.string.achievementOptionTutorial),ShowcaseView.BELOW_SHOWCASE);
                 break;
             case 17:
-                moveShowCaseCircle(rankingOptionTutorial,"Ranking","Aqui você tem acesso ao ranking de pontuação dos 10 primeiros exploradores, incluindo a sua!",ShowcaseView.BELOW_SHOWCASE);
+                moveShowCaseCircle(rankingOptionTutorial,getString(R.string.rankingOption),getString(R.string.rankingOptionTutorial),ShowcaseView.BELOW_SHOWCASE);
                 break;
             case 18:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.ranking_image, null));
-                moveShowCaseCircle(rankingTutorial,"Ranking","Pode-se obervar os 10 primeros colocados e você aparecendo de forma destacada!",ShowcaseView.BELOW_SHOWCASE);
+                moveShowCaseCircle(rankingTutorial,getString(R.string.rankingTutorialTitle),getString(R.string.rankingTutorial),ShowcaseView.BELOW_SHOWCASE);
                 break;
             case 19:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.open_menu_image, null));
-                moveShowCaseCircle(mapOptionTutorial,"Mapa de Progresso","Aqui você tem acesso a mesma tela acessada através do botão mapa já mencionado!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(mapOptionTutorial,getString(R.string.mapOption),getString(R.string.mapOptionTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 20:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.open_menu_image, null));
-                moveShowCaseCircle(preferenceOptionTutorial,"Prefências","Aqui você tem acesso a algumas opções sobre sua conta!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(preferenceOptionTutorial,getString(R.string.preferenceOption),getString(R.string.preferenceOptionTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 21:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.preference_screen_image, null));
-                moveShowCaseCircle(preferenceEditNickTutorial,"Editar Nickname","Aqui você pode editar o seu Nickname!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(preferenceEditNickTutorial,getString(R.string.preferenceEditNick),getString(R.string.preferenceEditNickTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 22:
-                moveShowCaseCircle(preferenceDeleteAccountTutorial,"Deletar Conta","Aqui você pode deletar sua conta!\nLembre-se que esse processo é irreversível!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(preferenceDeleteAccountTutorial,getString(R.string.preferenceDeleteAccount),getString(R.string.preferenceDeleteAccountTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 23:
-                moveShowCaseCircle(preferenceSignOutTutorial,"Deslogar do App","Aqui você pode deslogar da sua conta!\nLembre-se que ao deslogar, você perderá todos o seu progresso atual da história, mas seus elementos não serão perdidos!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(preferenceSignOutTutorial,getString(R.string.preferenceSignOut),getString(R.string.preferenceSignOutTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 24:
                 tutorialCurrentImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.open_menu_image, null));
-                moveShowCaseCircle(tutorialOptionTutorial,"Tutorial","Você está nessa opção, e pode acessar o tutorial através dela quando quiser!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(tutorialOptionTutorial,getString(R.string.tutorialOption),getString(R.string.tutorialOptionTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 25:
-                moveShowCaseCircle(aboutOptionTutorial,"Informações Sobre o App","E por último, aqui você pode acessar algumas informações adicionais sobre o Missão Nascente!",ShowcaseView.ABOVE_SHOWCASE);
+                moveShowCaseCircle(aboutOptionTutorial,getString(R.string.aboutOption),getString(R.string.aboutOptionTutorial),ShowcaseView.ABOVE_SHOWCASE);
                 break;
             case 26:
                 tutorialCurrentImage.setBackgroundColor(0x00000000);
                 tutorialCurrentImage.setBorderWidth((float) 0);
                 showcaseView.setTarget(Target.NONE);
-                showcaseView.setContentTitle("Tutorial Concluído");
-                showcaseView.setContentText("Tenha uma espetacular aventura, Explorador(a)!");
+                showcaseView.setContentTitle(getString(R.string.tutorialCompleted));
+                showcaseView.setContentText(getString(R.string.tutorialCompletedMessage));
                 showcaseView.setStyle(R.style.Final);
-                showcaseView.setButtonText("Fim");
+                showcaseView.setButtonText(getString(R.string.theEnd));
                 break;
             case 27:
                 Intent mainScreenIntent = new Intent(TutorialScreenActivity.this, MainScreenActivity.class);
@@ -176,12 +176,12 @@ public class TutorialScreenActivity extends AppCompatActivity implements View.On
         showcaseView = new ShowcaseView.Builder(this)
                 .setTarget(Target.NONE)
                 .setOnClickListener(this)
-                .setContentTitle("Tutorial")
+                .setContentTitle(getString(R.string.tutorialOption))
                 .setContentText(R.string.initialMessageTutorial)
                 .setStyle(R.style.Transparency)
                 .build();
 
-        showcaseView.setButtonText("próximo");
+        showcaseView.setButtonText(getString(R.string.next));
     }
 
     private void moveShowCaseCircle(Target currentTarget,String title,String text, int position){
